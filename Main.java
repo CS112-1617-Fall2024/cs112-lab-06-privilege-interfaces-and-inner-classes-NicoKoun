832b18b829af673f3966af98ab6faa0d5c740cf6
NicoKoun <108241057+NicoKoun@users.noreply.github.com>
2024-10-15 05:46:03 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.jar.Attributes.Name;
 
 public class Main
 {
@@ -34,38 +35,38 @@ public class Main
 	public static void main(String[] args)
 	{
 		// DECLARATION + INITIALIZATION
-		Person p1 = new Person("Amira", "I am a Syrian refugee.", 40);
-		Person p2 = new Person("D'Andra", "I am an African-American trans woman.", -20);
-		Person p3 = new Person("Jennifer", "I am a New Yorker", 140);
-		Person p4 = new Person("Pete", "I am a guy from Pennsylvania", 200);
+		Person p1 = new Person("Amira", "she/her/hers", "I am a Syrian refugee.", 40);
+		Person p2 = new Person("D'Andra", "she/her/hers", "I am an African-American trans woman.", -20);
+		Person p3 = new Person("Jennifer", "ze/hir/hirs","I am a New Yorker", 140);
+		Person p4 = new Person("Pete", "he/him/his", "I am a guy from Pennsylvania", 200);
 		Person self = new Person();
-		Person[] people = {p1, p2, p3, p4, self};
+		Person[] people = { p1, p2, p3, p4, self };
 		boolean done = false;
 		int input;
-		
+
 		// WELCOME + INTRO
 		System.out.println("Welcome to the Privilege Calculator.\n\n"
 				+ "This is a small exercise that gives us a glimpse at how "
 				+ "fortunate we have been in life.\n");
 
 		Main.fillInfo(self);
-		
+
 		// INPUT + CALCULATION + OUTPUT
-		do{
+		do {
 			System.out.println("\n~~~Main Menu~~~\n");
 			System.out.println("1. Take questionnaire to calculate privilege estimate.");
-			System.out.println("2. Check my estimate. (Defaults to " + Person.DEFAULT_PRIVILEGE + " if questionnaire has not been taken.)");
+			System.out.println("2. Check my estimate. (Defaults to " + Person.DEFAULT_PRIVILEGE
+					+ " if questionnaire has not been taken.)");
 			System.out.println("3. Compare my estimate with others'.");
 			System.out.println("4. Exit program.");
 			System.out.print("What would you like to do?\nEnter choice: ");
-			
+
 			input = keyboard.nextInt();
 			System.out.println();
 
-			switch(input)
-			{
+			switch (input) {
 				case 1:
-					self.setPrivilege( Main.doPrivilegeQuestionnaire() );
+					self.setPrivilege(Main.doPrivilegeQuestionnaire());
 					System.out.println("Your privilege estimate is: " + self.getPrivilege());
 					System.out.println("\nReturning to main menu...\n");
 					break;
@@ -74,7 +75,7 @@ public class Main
 					break;
 				case 3:
 					/***** TODO: (Part 1) implement a comparison case using the comparable method on the Person class to compare self to p1-p4*****/
-					
+					comparePeople(people);
 					System.out.println("\nReturning to main menu.\n");
 					break;
 				case 4:
@@ -85,29 +86,95 @@ public class Main
 				default:
 					System.out.println("Invalid input, please enter a valid choice."
 							+ "\nReturning to main menu...\n");
-					break;				
+					break;
 			}
-		}while(!done);
+		} while (!done);
 
 		System.out.println("Thank you for exploring your privilege, it can be uncomfortable but it's a crucial step" +
-			"in our own growth and self-reflection. We appreciate you taking that journey with us! :D");
+				"in our own growth and self-reflection. We appreciate you taking that journey with us! :D");
+	}
+	
+	public static void comparePeople(Person[] group) {
+		Person user = group[group.length - 1];
+		int compare;
+		printRuler();
+		for (Person p : group)
+			printPrivilegebar(p);
+
+		System.out.println("\nSummary of privilege estimate:");
+		for (int i = 0; i < group.length - 1; i++) {
+			compare = user.compareTo(group[i]);
+			if (compare > 0) {
+				System.out.println("More privilege than " + group[i].getName());
+			} else if (compare == 0) {
+				System.out.println("Same privilege as " + group[i].getName());
+			} else {
+				System.out.println("Less privilege as " + group[i].getName());
+			}
+		}
 	}
 
-	/***** TODO: (Part 2) upgrade method to ask user for pronouns and background info *****/
+	public static void printRuler() {
+		int total = LEFT_WIDTH + 1 + RIGHT_WIDTH;
+		System.out.printf("%-" + NAME_WIDTH + "s%-" + LEFT_WIDTH + "d0%" + RIGHT_WIDTH + "d%n", "", MIN, MAX);
+		System.out.printf("%-" + NAME_WIDTH + "s", "");
+		for (int i = 0; i < total; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
+		System.out.printf("%-" + NAME_WIDTH + "s%-" + LEFT_WIDTH + "s%1s%" + RIGHT_WIDTH + "s%n", "NAME (ESTIMATE):", "|", "|", "|");
+	}
+
+	public static void printPrivilegebar(Person p){
+		//System.out.printf("%s (%d) %s%n", p.getName(), p.getPrivilege(), p.getName().charAt(0));
+		int privilegeEstimate = p.getPrivilege(), position = Math.abs(privilegeEstimate) / 10;
+		char marker = p.getName().charAt(0);
+		String label = p.getName() + " (" + privilegeEstimate + ")";
+		System.out.printf("%-" + NAME_WIDTH + "s", label);
+		if (privilegeEstimate < 0) {
+			if ((LEFT_WIDTH - position) == 0) {
+				System.out.printf("%-" + LEFT_WIDTH + "s", marker);
+			} else {
+				System.out.printf("%" + (LEFT_WIDTH-position) + "s%-" + position + "s", "", marker);
+			}
+		} else {
+			System.out.printf("%" + LEFT_WIDTH + "s", "");
+		}
+		if (privilegeEstimate == 0) {
+			System.out.print(marker);
+		} else {
+			System.out.print(" ");
+		}
+		if (privilegeEstimate > 0) {
+			if ((RIGHT_WIDTH - position) == 0) {
+				System.out.printf("%-" + RIGHT_WIDTH + "s", marker);
+			} else {
+				System.out.printf("%" + position + "s%-" + (RIGHT_WIDTH-position) + "s", marker, "");
+			}
+		} else {
+			System.out.printf("%" + RIGHT_WIDTH + "s", "");
+		}
+		System.out.println();
+}	
+
+	/*** TODO: (Part 2) upgrade method to ask user for pronouns and background info ***/
 	public static void fillInfo(Person person){
 		//sets default privilege prior to questionnaire to 100
-		String name, story;
+		String name, pronouns, background;
 		
 		System.out.println("What is your name? ");
 		name = keyboard.nextLine();
-		System.out.println("\nHello " + name + ", write a small self-identifying statement about yourself "
+		System.out.println("\nHello " + name + "!");
+		System.out.println("Here is a list of common examples of perferred pronouns: \nGender Neutral/Nonbinary: they/them/theirs or ze/hir/hirs\nFeminine: she/her/hers\nMasculine: he/him/his");
+		pronouns = keyboard.nextLine();
+		System.out.println("Please share a small self-identifying statement about yourself "
 				+ "and your background and identity, this can be anything you like!\n"
 				+ "For example: I'm a [nationality / place of origin / ethnicity / sexuality / gender expression / etc.]...");
-		System.out.println("Tell us about yourself: ");
-		story = keyboard.nextLine();
+		background = keyboard.nextLine();
 		
 		person.setName(name);
-		person.setStory(story);
+		person.setPronouns(pronouns);
+		person.setBackground(background);
 	}
 
 	public static int doPrivilegeQuestionnaire() {
--- Person.java
+++ Person.java
@@ -1,78 +1,223 @@
 public class Person implements Comparable
 {
-	/***** TODO: (Part 2) create helper inner class for Identity*****/
+	/**
+    * this class is used to represent you as a person according to multiple labels you provide
+    * 
+    * @author Nico Koundakjian
+    * @version 1.0
+    */
+	private class Identity {
+		/**
+		* this class is used to store your identity
+		* 
+		* @author Nico Koundakjian
+		* @version 1.0
+		*/
+		
+		/*Instance Variables
+		 * String pronouns: your pronouns
+		 * String background: your background
+		 */
+		private String pronouns;
+		private String background;
 
+		//Constructors
+    	/*
+    	 * Default constructor: sets each of the variables to default values
+     	 * @param: none
+    	 * @return: none
+    	 */
+		public Identity() {
+			this(DEFAULT_PRONOUNS, DEFAULT_BACKGROUND);
+		}
+  		/*
+    	 * Parameterized constructor: sets each of the variables to the set parameters
+    	 * @param: String p: the String that will be set to pronouns, String B: the String that will be set to background
+     	 * @return: none
+     	*/
+		public Identity(String P, String B) {
+			this.pronouns = P;
+			this.background = B;
+		}
+		/**
+    	 * ToString: the string that will apears when the Identity is printed
+    	 * @param none
+    	 * @return String: a string printing all of the data
+    	*/
+		@Override
+		public String toString() {
+			return "my personal pronouns are " + this.pronouns + " and " + this.background;
+		}
+		/**
+    	 * equals: determines if a Identity has the same values as another object
+    	 * @param object o: the other object that will be compaired
+    	 * @return boolean: returns true if the objects are equal, returns false otherwise and if o is null or not a Identity
+    	 */
+		@Override
+		public boolean equals(Object other){	
+			if(other == null || (!(other instanceof Identity))) {
+		    	return false;
+			}
+		
+			Identity otherIdentity = (Identity) other;
+			return this.pronouns.equals(otherIdentity.pronouns) && this.background.equals(otherIdentity.background);
+		}
+	}// end of Identity inner class
 	// CONSTANT VARIABLES
+	/*
+	 * String DEFAULT_NAME: the default name of the person
+	 * String DEFAULT_PRONOUNS: the default pronouns of the person
+	 * String DEFAULT_BACKGROUND:the default background of the person
+	 * int DEFAULT_PRIVILEGE: the default amount of privilege the person has
+	 */
 	public static final String DEFAULT_NAME = "Jamie Doe";
-	public static final String DEFAULT_STORY =  "Unknown";
+	public static final String DEFAULT_PRONOUNS = "they/them/theirs";
+	public static final String DEFAULT_BACKGROUND =  "Unknown";
 	public static final int DEFAULT_PRIVILEGE = 100;
 
 	// INSTANCE VARIABLES
-	private String name, story;
+	/*
+	 * String name: the name of the person
+	 * Identity story: the persons identity that contains pronouns and background
+	 * int priviliege: the amount of privilege the person has
+	 */
+	private String name;
+	private Identity story = new Identity();
 	private int privilege;
 
 	// CONSTRUCTORS	
-	public Person(String name, String story, int privilege) {
-		this.setAll(name, story, privilege);
-	}
-		
-	public Person() {
-		this(DEFAULT_NAME, DEFAULT_STORY, DEFAULT_PRIVILEGE);
-	}
+	/*
+     * Parameterized constructor: sets each of the variables to the set parameters
+     * @param: String name: the String that will be set to name, String pronouns: the String that will be set to story.pronouns, String backgrounds: the String that will be set to story.background, int privilege: the int that will be set to privilege
+     * @return: none
+    */
 	
+	public Person(String name, String pronouns, String background, int privilege) {
+		this.setAll(name, pronouns, background, privilege);
+	}
+	/*
+     * Default constructor: sets each of the variables to default values
+     * @param: none
+     * @return: none
+     */
+	public Person() {
+		this(DEFAULT_NAME, DEFAULT_PRONOUNS, DEFAULT_BACKGROUND, DEFAULT_PRIVILEGE);
+	}
+	/**
+     * copy constructor: makes a shallow copy of another Person object
+     * @param Person original: the Person that will be copied
+     * @return: none
+     */
 	public Person(Person original) {
 		if(original == null) {
 			throw new IllegalArgumentException("Cannot copy null obect in Person copy constructor");
 		} else {
-			this.setAll(original.name, original.story, original.privilege);
+			this.setAll(original.name, original.story.pronouns, original.story.background, original.privilege);
 		}
 	}
 
 	// MUTATORS/SETTERS
+	/**
+     * setName: will set the name of the Person
+     * @param String Name: the new name of the Person
+     * @return none
+     */
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public void setStory(String story) {
-		this.story = story;
+	/**
+     * setPronouns: will set the pronouns of the Person's story identity
+     * @param String pronouns: the new pronouns of the Person's story identity
+     * @return none
+     */
+	public void setPronouns(String pronouns) {
+		this.story.pronouns = pronouns;
 	}
-
+	/**
+     * setBackground: will set the background of the Person's story identity
+     * @param String background: the new background of the Person's story identity
+     * @return none
+     */
+	public void setBackground(String background) {
+		this.story.background = background;
+	}
+	/**
+     * setPrivilege: will set the privilege of the Person
+     * @param String privilege: the new privilege of the Person
+     * @return none
+     */
 	public void setPrivilege(int privilege) {
 		this.privilege = privilege;
 	}
-
-	public void setAll(String name, String story, int privilege) {
+	/**
+     * setAll: will set the all of the instance variables of the Person
+     * @param String Name: the new name of the Person, String pronouns: the new pronouns of the Person's story identity, String background: the new background of the Person's story identity, String privilege: the new privilege of the Person
+     * @return none
+     */
+	public void setAll(String name, String pronouns, String background, int privilege) {
 		this.setPrivilege(privilege);
 		this.setName(name);
-		this.setStory(story);
+		this.setPronouns(pronouns);
+		this.setBackground(background);
 	}
 
 	// ACCESSORS / GETTERS
+	/**
+     * getName: will return the name of Person
+     * @param none
+     * @return String: name
+     */
 	public String getName() {
 		return this.name;
 	}
-		
-	public String getStory() {
-		return this.story;
+	
+	/**
+	 * getBackground: will return the background of Person's story identity
+	 * @param none
+	 * @return String: background
+	 */
+	public String getBackground() {
+		return this.story.background;
 	}
-
+	/**
+     * getPronouns: will return the pronouns of Person's story identity
+     * @param none
+     * @return String: pronouns
+     */
+	public String getPronouns() {
+		return this.story.pronouns;
+	}
+	/**
+     * getName: will return the privilege of Person
+     * @param none
+     * @return int: privilege
+     */
 	public int getPrivilege() {
 		return this.privilege;
 	}
 
 	// OTHER REQUIRED METHODS
+	/**
+     * ToString: the string that will apears when the Person is printed
+     * @param none
+     * @return String: a string printing all of the data
+    */
 	@Override
 	public String toString()
 	{
-		return "My name is "+ this.name + " and "+ this.story + "\n"
+		return "My name is "+ this.name + this.story + "\n"
 				+ "According to this calculator I ended up with "+ this.privilege + " estimated privilege points";
 	}
-	
+	/**
+    * equals: determines if a Person has the same values as another object
+    * @param object o: the other object that will be compaired
+    * @return boolean: returns true if the objects are equal, returns false otherwise and if o is null or not a Person
+    */
 	@Override
 	public boolean equals(Object other) 
 	{
 		if(other == null || (!(other instanceof Person))) {
-		      return false;
+		    return false;
 		}
 		
 		Person otherPerson = (Person) other;
@@ -81,7 +226,11 @@ public class Person implements Comparable
 	}
 
 	// INTERFACE METHODS
-	/***** TODO: (Part 1) To method to implement Comparable interface*****/
+	/**
+     * compareTo: compares a Person to another Person object based on the natural ordering of people for this project, based on their privilege estimate
+     * @param object other: the other object that will be compaired
+     * @return int: returns 1 if this person has more privilege than the other, returns 0 if the privileges are equal, returns -1 if this person has less privilege than the other
+    */
 	@Override
 	public int compareTo(Object other) {
 		if (other == null || (!(other instanceof Person))) {
